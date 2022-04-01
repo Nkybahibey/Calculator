234cda964be46b487cbc9aab7954a8e071e6fc1f
nkybahibey <odiinkechinyere@gmail.com>
2022-04-01 09:41:32 +0100

    first commit

---
+++ nky.py
@@ -0,0 +1,25 @@
+print("Select an operation to perform:")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPY")
+print("4. DIVIDE")
+  
+operation = input()
+if operation == "1":
+    num1 = input  ("Enter first number: ") 
+    num2 = input  ("Enter second number: ")
+    print("The sum is " + str(int(num1) + int(num2)))
+elif operation == "2":    
+    num1 = input ("Enter first number: ")
+    num2  =input ("Enter second number: ")
+    print(" The differnce is " + str(int(num1) - int(num2)))
+elif operation == "3":
+     num1 = input("Enter first number: " )
+     num2 = input("Enter second number: ")
+     print("The product is "+ str(int(num1 ) * int(num2)))
+elif operation == "4":
+     num1 = input("Enter second number: ")
+     num2 = input("Enter second number: ")
+     print("The result is "+ str(int(num1) / int(num2)))
+else:
+    print("Invalid Entry")
